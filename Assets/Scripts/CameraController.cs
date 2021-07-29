3dd2de63f186f7c9d5fe869b32c0af2e5270d5da
Akira-Ito0109 <syobonnito@gmail.com>
2021-07-29 20:06:22 +0900

    手順6まで

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject playerObj;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - playerObj.transform.position;
+    }
+
+    private void Update()
+    {
+        if(playerObj !=null)
+        {
+            transform.position = playerObj.transform.position + offset;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [Header("移動速度")]
+    public float moveSpeed;
+
+    [Header("落下速度")]
+    public float fallSpeed;
+
+    private Rigidbody rb;
+
+    private float x;
+    private float z;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        Debug.Log(x);
+        Debug.Log(z);
+
+        rb.velocity = new Vector3(x * moveSpeed, -fallSpeed, z * moveSpeed);
+
+        Debug.Log(rb.velocity);
+    }
+}
