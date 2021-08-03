707c00d02aa1e2ca48ea4f94092251f5147bc817
Akira-Ito0109 <syobonnito@gmail.com>
2021-08-03 19:23:10 +0900

    手順12まで終了

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
@@ -10,56 +12,90 @@ public class PlayerScript : MonoBehaviour
     [Header("落下速度")]
     public float fallSpeed;
 
-    [Header("着水判定用。trueなら着水済み")]
+    [Header("着水判定用。trueなら着水済")]
     public bool inWater;
-    
 
     private Rigidbody rb;
 
     private float x;
     private float z;
 
+
+    private Vector3 straightRotation = new Vector3(180, 0, 0);
+
+    private int score;
+
+
+
     [SerializeField, Header("水しぶきのエフェクト")]
-    private GameObject splashEffectPrefab = null;
+    private GameObject waterEffectPrefab = null;
 
     [SerializeField, Header("水しぶきのSE")]
     private AudioClip splashSE = null;
 
-    private void Start()
+    [SerializeField]
+    private Text txtScore;
+
+
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+
+        transform.eulerAngles = straightRotation;  
+
+
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         x = Input.GetAxis("Horizontal");
         z = Input.GetAxis("Vertical");
 
-       // Debug.Log(x);
-       // Debug.Log(z);
-
-        //rb.velocity = new Vector3(x * moveSpeed, -fallSpeed, z * moveSpeed);
-        rb.velocity = new Vector3(x, rb.velocity.y, z).normalized * moveSpeed;
-
-       // Debug.Log(rb.velocity);
+        rb.velocity = new Vector3(x * moveSpeed, -fallSpeed, z * moveSpeed);
     }
+
 
     private void OnTriggerEnter(Collider col)
     {
-        if(col.gameObject.tag=="Water"&&inWater==false)
+
+        if (col.gameObject.tag == "Water" && inWater == false)
         {
+
             inWater = true;
 
-            GameObject effect = Instantiate(splashEffectPrefab, transform.position, Quaternion.identity);
-
+            GameObject effect = Instantiate(waterEffectPrefab, transform.position, Quaternion.identity);
             effect.transform.position = new Vector3(effect.transform.position.x, effect.transform.position.y, effect.transform.position.z - 0.5f);
 
             Destroy(effect, 2.0f);
 
             AudioSource.PlayClipAtPoint(splashSE, transform.position);
 
-            Debug.Log("着水:" + inWater);
+
+            StartCoroutine(OutOfWater());
+        }
+        if (col.gameObject.tag == "FlowerCircle")
+        {
+
+            score += col.transform.parent.GetComponent<FlowerCircle>().point;
+
+            txtScore.text = score.ToString();
+
         }
     }
+
+
+
+    private IEnumerator OutOfWater()
+    {
+        yield return new WaitForSeconds(1.0f);
+
+        rb.isKinematic = true;
+
+        transform.eulerAngles = new Vector3(-30, 180, 0);
+
+        transform.DOMoveY(4.7f, 1.0f);
+    }
+
 
 }
