fff8c5468e28f10019665e19c0b755c5cf6734d7
Akira-Ito0109 <syobonnito@gmail.com>
2021-08-03 19:45:31 +0900

    手順13

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Penguin;
+
+    [SerializeField]
+    private GameObject Goal;
+
+    private float distance;
+
+    private void Update()
+    {
+        //ペンギンと水面のy座標の差を距離とする
+        distance = Penguin.transform.position.y - Goal.transform.position.y;
+
+
+        if(distance<0)
+        {
+            distance = 0;
+        }
+
+        Debug.Log(distance);
+    }
+}
